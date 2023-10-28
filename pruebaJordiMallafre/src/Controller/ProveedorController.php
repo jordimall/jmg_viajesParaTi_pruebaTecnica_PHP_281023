d4af9849a5c6dc0c2f85ee1d0a0e6bb0a23cd75b
jordimall <jordi.mallafre@hotmail.com>
2023-10-28 17:49:48 +0200

    Se añade paginación

--- pruebaJordiMallafre/src/Controller/ProveedorController.php
+++ pruebaJordiMallafre/src/Controller/ProveedorController.php
@@ -6,16 +6,24 @@ use App\Repository\ProveedorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProveedorController extends AbstractController
 {
     /**
      * @Route("/proveedor", name="app_proveedor")
      */
-    public function index(ProveedorRepository $proveedorRepository): Response
+    public function index(ProveedorRepository $proveedorRepository, Request $request, PaginatorInterface $paginator): Response
     {
+        $pagination = $paginator->paginate(
+            $proveedorRepository->findAll(), // Obtén todos los proveedores
+            $request->query->getInt('page', 1), // Página solicitada, por defecto página 1
+            10 // Número de elementos por página
+        );
+    
         return $this->render('proveedor/index.html.twig', [
-            'proveedors' => $proveedorRepository->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 }
